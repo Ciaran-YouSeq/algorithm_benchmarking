9429749e24c49489f3e7f7bbdedd01dde95b5d26
Ciaran-Lundy <ciaranlundy@gmail.com>
2020-11-14 12:57:15 +0000

    removing valgrind

--- yaml_parser.py
+++ yaml_parser.py
@@ -2,6 +2,7 @@
 
 import argparse
 import yaml
+import os
 import sys
 sys.path.insert(1, './benchmarks/')
 
@@ -25,19 +26,55 @@ def get_args():
 
 config = get_args()
 
-def parse_config(config):
+def get_benchmarks():
+    """find all benchmarks"""
     benchmarks = []
-    for benchmark_settings in config:
-        benchmark_settings_section = list(benchmark_settings.keys())[0]
-        for benchmark in "general_settings", "timed_command", "thread_scaling", "memory_footprint", "settings_optimizer":
-            if benchmark_settings_section == benchmark:
-                exec('import {}'.format(benchmark))
-                benchmarks.append(eval(benchmark).parse_settings(benchmark_settings))
-    #if list item starts with X, send this section to benchmark script for X
+    for benchmark in os.listdir("./benchmarks/"):
+        if ".py" in benchmark:
+            benchmarks.append(benchmark.split(".")[0])
     return benchmarks
 
-print(parse_config(config))
 
+def load_benchmarks(config, benchmarks):
+    """parse config file, and load benchmarks from ./benchmarks/"""
+    loaded_benchmarks = []
+    for benchmark_settings in config:
+        benchmark_settings_section = list(benchmark_settings.keys())[0]
+        print(benchmark_settings)
+        for benchmark in benchmarks:
+            if benchmark_settings_section == benchmark:
+                exec('import {}'.format(benchmark))
+                #loaded_benchmarks.append(eval(benchmark).parse_settings(benchmark_settings))
+                loaded_benchmarks.append(exec("{benchmark}.{benchmark}".format(benchmark=benchmark)))
+                #loaded_benchmarks.append(exec("{benchmark}.{benchmark}.parse_settings({benchmark_settings})".format(benchmark=benchmark, benchmark_settings=benchmark_settings)))
+    #if list item starts with X, send this section to benchmark script for X
+    return loaded_benchmarks
+
+def parse_settings(config, loaded_benchmarks):
+    parsed_settings = []
+    print(config)
+    for benchmark_settings in config:
+        benchmark_settings_section = list(benchmark_settings.keys())[0]
+    for benchmark in loaded_benchmarks:
+        parsed_settings.append(exec("{benchmark}.parse_settings({benchmark_settings})".format(benchmark=benchmark, benchmark_settings=benchmark_settings)))
+
+
+def run_benchmarks(loaded_benchmarks):
+    print(loaded_benchmarks)
+    for benchmark in loaded_benchmarks:
+        print(benchmark)
+        exec("{}.run(settings)".format(benchmark.keys()[0]))
+
+#{'general_settings': {'algorithm_name': 'bwa', 'algorithm_version': '1.0.0'}}
+
+print("get benchmarks")
+benchmarks = get_benchmarks()
+print("loaded benchmarks")
+loaded_benchmarks = load_benchmarks(config, benchmarks)
+print("parsed benchmarks")
+parse_settings = parse_settings(config, loaded_benchmarks)
+print("run benchmarks")
+run_benchmarks(loaded_benchmarks)
 
 # for thing in thing:
 #run thing with settings_dict
