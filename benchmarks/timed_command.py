fbcfe51b4f2eba287398127ee3d1c62d45f94c48
Ciaran-Lundy <ciaranlundy@gmail.com>
2020-11-14 16:17:44 +0000

    adding command timer to timed command benchmark

--- benchmarks/timed_command.py
+++ benchmarks/timed_command.py
@@ -3,13 +3,25 @@
 import argparse
 import yaml
 import base_benchmark
+import time
+import subprocess
+import sys
 
 class timed_command(base_benchmark.base_benchmark):
     def __init__(self):
         base_benchmark.base_benchmark.__init__(self)
         self.name = __class__.__name__
 
-    def run(self):
+    def run_benchmark(self):
         print("running " + self.get_name())
         print(self.settings)
-        
+        command = self.settings['command']
+        start = time.time()
+        with subprocess.Popen(command, stdout=sys.stderr, stderr=subprocess.PIPE, universal_newlines=True, shell=True) as process:
+                _, stderr = process.communicate()
+                if process.returncode != 0:
+                    print(stderr.splitlines()[:-1], file=sys.stderr)
+                    raise self.ExecutionError(command, process.returncode)
+        end = time.time() - start
+        print(end)
+        return end
