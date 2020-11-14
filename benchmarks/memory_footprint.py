2cd833ba6fc20283e0645ac020c279e106224678
Ciaran-Lundy <ciaranlundy@gmail.com>
2020-11-14 23:23:11 +0000

    adding run_benchmark function, fixing run function

--- benchmarks/memory_footprint.py
+++ benchmarks/memory_footprint.py
@@ -3,13 +3,33 @@
 import argparse
 import yaml
 import base_benchmark
+import subprocess
+import sys
 
 class memory_footprint(base_benchmark.base_benchmark):
     def __init__(self):
         base_benchmark.base_benchmark.__init__(self)
         self.name = __class__.__name__
 
-    def run(self):
+    def run_command(self, command):
+        with subprocess.Popen(command, stdout=sys.stderr, stderr=subprocess.PIPE, universal_newlines=True, shell=True) as process:
+                stdout, stderr = process.communicate()
+                if process.returncode != 0:
+                    print(command)
+                    exit()
+
+    def run_benchmark(self):
         print("running " + self.get_name())
         print(self.settings)
-
+        command = "valgrind --tool=massif --massif-out-file=massif.out "+self.settings['command']
+        self.run_command(command)
+        command = "ms_print massif.out > massif.txt"
+        self.run_command(command)
+        with open("massif.txt", "r") as massif_output:
+            massif_output = massif_output.readlines()
+            data_extract = []
+            for line in massif_output:
+                if not line.startswith("|") and not line.startswith("->") and not line.endswith("etc.\n"):
+                    data_extract.append(line)
+        return data_extract
+        
