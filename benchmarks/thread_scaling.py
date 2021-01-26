311b0fdf6cee0084596767db37011aaabc556be5
Ciaran-Lundy <ciaranlundy@gmail.com>
2021-01-26 09:59:39 +0000

    adding in output setting to determine where output of alignment is stored

--- benchmarks/thread_scaling.py
+++ benchmarks/thread_scaling.py
@@ -19,14 +19,14 @@ class thread_scaling(base_benchmark.base_benchmark):
         threads_list = self.settings['threads'].split(',')
         results = {}
         for threads in threads_list:
-            threaded_command = command.format(threads=threads)
+            threaded_command = command.format(threads=threads, output=self.settings['output'])
             print(threaded_command)
             start = time.time()
             with subprocess.Popen(threaded_command, stdout=sys.stderr, stderr=subprocess.PIPE, universal_newlines=True, shell=True) as process:
                 _, stderr = process.communicate()
                 if process.returncode != 0:
                     print(stderr.splitlines()[:-1], file=sys.stderr)
-                    print("fucked it.")
+                    print("job failed.")
             end = time.time() - start
             print(threads, end)
             results[threads] = end
